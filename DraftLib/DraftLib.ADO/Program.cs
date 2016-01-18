480986554e924e4ed7539ba53f915d7a78ec8c26
David Harutyunyan <dharutyu@hotmail.com>
2016-01-18 10:33:56 -0800

    empty projects

---
+++ DraftLib/DraftLib.ADO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftLib.ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DraftLib/DraftLib.TestWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DraftLib.TestWeb.MvcApplication" Language="C#" %>
